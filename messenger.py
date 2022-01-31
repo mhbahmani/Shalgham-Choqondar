c1af21262a71dc13ae34f69c591719e6c0e007b8
mhbahmani <mhbahmani79@gmail.com>
2022-01-31 17:57:53 +0330

    add socket to MessengerServer

--- messenger.py
+++ messenger.py
@@ -1,5 +1,16 @@
+import logging
+import socket
+
 class MessengerServer:
+    server: socket.socket
+    HOST: str = '127.0.0.1'
+    PORT: int = 8001
+
     def __init__(self) -> None:
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.bind((MessengerServer.HOST, MessengerServer.PORT))
+        self.server.listen(1)
+        logging.info(f"MessengerServer is listening on {MessengerServer.HOST}:{MessengerServer.PORT}")
         self.users: list = []
 
     def signup(self):
